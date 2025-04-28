af9364f53d00c6e1914cf67e0a75abdce1107371
Daniel Ojeda <ojedadanielalejandro333@gmail.com>
2025-04-28 13:22:30 -0300

    fix: update redirect logic in Header component to include 'nosotros' for transportista path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
           redirectPath = "/formulario-choferes";
         } else if (txt.includes("comisionista")) {
           redirectPath = "/formulario-comisionista";
-        } else if (txt.includes("transportista")) {
+        } else if (txt.includes("transportista") || txt.includes("nosotros")) {
           redirectPath = "/formulario";
         }
 
